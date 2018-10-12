0792362629ad703311e8cd282b30b6b348cb2f1d
Josélia L. Costa <joselialcosta@gmail.com>
2018-10-12 13:09:46 -0300

    Transform ListContacts in stateless functional component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 
-class ListContacts extends Component {
-  render() {
+function ListContacts(props) {
     return (
       <ol className='contact-list'>
-        {this.props.contacts.map((contact) => (
+        {props.contacts.map((contact) => (
           <li className='contact-list-item'>
             <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL})` }} />
             <div className='contact-details'>
@@ -19,7 +18,6 @@ class ListContacts extends Component {
         }
       </ol>
     )
-  }
 }
 
 export default ListContacts
