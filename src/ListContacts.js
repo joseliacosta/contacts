2d736a9db167b1452063b001c5b23e331df8e8f7
Josélia L. Costa <joselialcosta@gmail.com>
2018-10-14 00:18:06 -0300

    Transform functional component in class to implement state and behavior

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,29 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { PropTypes } from 'prop-types';
 
-function ListContacts(props) {
-    return (
-      <ol className='contact-list'>
-        {props.contacts.map((contact) => (
-          <li className='contact-list-item' key={contact.id}>
-            <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL})` }} />
-            <div className='contact-details'>
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button className='contact-remove' onClick={()=>props.onDeleteContact(contact)}>
-              Remove
-            </button>
-          </li>
-        ))
-        }
-      </ol>
-    )
-}
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
 
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
+  render() {
+    return(
+      <div>
+        <ol className='contact-list'>
+          {props.contacts.map((contact) => (
+            <li className='contact-list-item' key={contact.id}>
+              <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL})` }} />
+              <div className='contact-details'>
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <button className='contact-remove' onClick={()=>props.onDeleteContact(contact)}>
+                Remove
+              </button>
+            </li>
+          ))
+          }
+        </ol>
+      </div>
+    )
+  }
 }
 
 export default ListContacts
