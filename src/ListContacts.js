d53b5e402be2d95dc795c2463bcfc2b3a8c35fef
Josélia L. Costa <joselialcosta@gmail.com>
2018-10-14 12:39:37 -0300

    Implement search and filter by names

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { PropTypes } from 'prop-types';
+import { PropTypes } from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class ListContacts extends Component {
   static propTypes = {
@@ -14,6 +16,14 @@ class ListContacts extends Component {
   }
 
   render() {
+    let showingContacts
+    if (this.state.query) {
+      const match = new RegExp(escapeRegExp(this.state.query), 'i')
+      showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+    } else {
+      showingContacts = this.props.contacts
+    }
+
     return(
       <div className='list-contacts'>
         <div className='list-contacts-top'>
@@ -26,7 +36,7 @@ class ListContacts extends Component {
           />
         </div>
         <ol className='contact-list'>
-          {this.props.contacts.map((contact) => (
+          {showingContacts.map((contact) => (
             <li className='contact-list-item' key={contact.id}>
               <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL})` }} />
               <div className='contact-details'>
