ea493ea12cbbf47d6fc52fd47c97b2a650c0c8a7
Josélia L. Costa <joselialcosta@gmail.com>
2018-10-14 00:27:14 -0300

    Create element to search and associate it to react state

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -6,19 +6,34 @@ class ListContacts extends Component {
     contacts: PropTypes.array.isRequired,
     onDeleteContact: PropTypes.func.isRequired
   }
+  state = {
+    query: ''
+  }
+  updateQuery(query) {
+    this.setState({ query: query.trim() })
+  }
 
   render() {
     return(
-      <div>
+      <div className='list-contacts'>
+        <div className='list-contacts-top'>
+          <input
+            className='search-contacts'
+            type='text'
+            placeholder='Search contacts'
+            value={this.state.query}
+            onChange={event => { this.updateQuery(event.target.value) }}
+          />
+        </div>
         <ol className='contact-list'>
-          {props.contacts.map((contact) => (
+          {this.props.contacts.map((contact) => (
             <li className='contact-list-item' key={contact.id}>
               <div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL})` }} />
               <div className='contact-details'>
                 <p>{contact.name}</p>
                 <p>{contact.email}</p>
               </div>
-              <button className='contact-remove' onClick={()=>props.onDeleteContact(contact)}>
+              <button className='contact-remove' onClick={() => this.props.onDeleteContact(contact)}>
                 Remove
               </button>
             </li>
