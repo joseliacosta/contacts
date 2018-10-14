1dd132773956d4a629ee36cf8e9a5b21981eda96
Josélia L. Costa <joselialcosta@gmail.com>
2018-10-14 20:05:43 -0300

    Fix typo

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRoute } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRoute>
+  <BrowserRouter>
     <App />
-  </BrowserRoute>,
+  </BrowserRouter>,
   document.getElementById('root'));
   registerServiceWorker();
